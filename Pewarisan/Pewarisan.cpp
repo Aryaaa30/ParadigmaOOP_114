69535c89bba12a09c672e12a5d3f54128ec80488
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-05-22 16:25:50 +0700

    Membuat Prosedur berupa class orang dan memasukkan data

--- Pewarisan/Pewarisan.cpp
+++ Pewarisan/Pewarisan.cpp
@@ -2,7 +2,25 @@
 #include <string>
 using namespace std;
 
-class orang
+class orang {
+public :
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+        
+
+    
+
+    
+
+
+};
 
 int main()
 {
